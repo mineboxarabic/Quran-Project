704f24719be2aa6152785c49ecdcb3e808cdd30d
Yassin YOUNES <mineboxarabic@gmail.com>
2022-10-14 07:29:48 +0200

    Adding a ComboBox to a Form

--- src/Quran/Page.js
+++ src/Quran/Page.js
@@ -5,7 +5,7 @@ export class Page
 {
     async fillSourah(Sourah)
     {
-        const quran = new Quran()
+        let quran = new Quran();
         if(quran.isSourah(Sourah))
         {
             let sourahArray = await quran.getSourah(Sourah)
@@ -21,15 +21,41 @@ export class Page
             })
         }
     }
+    async createComboBox()
+    {
+        let body = document.querySelector('body'); // get the body element
+        this.ComboBox = document.createElement('select') // create select element <ComboBox> to select a sourah from the page of the quran
+        this.ComboBox.setAttribute('class','ComboBoxSourahs') // set class name for the combobox element
+
+        let sourahs = await this.summury.getSummury()
+        sourahs.forEach(sourah => {
+            let option = document.createElement('option')
+            option.setAttribute('value',sourah)
+            option.textContent = sourah
+            this.ComboBox.appendChild(option)
+        })
+        body.appendChild(this.ComboBox);
+
+    }
     constructor(Sourah,body)
     {
+        this.quran = new Quran();
+        this.summury = new Summury();
+
+        this.createComboBox();
         this.fillSourah(Sourah)
-        this.page = document.createElement('div')
-        this.page.setAttribute('class','page')
-        this.quranSection = document.createElement('div')
-        this.quranSection.setAttribute('class','quranSection')
-        this.backButton = document.createElement('button')
-        this.backButton.setAttribute('class','backButton')
+
+        
+        this.page = document.createElement('div') // create div element <page> to contain the page of the quran
+        this.page.setAttribute('class','page') // set class name for the page element
+
+
+        this.quranSection = document.createElement('div') // create div element <quranSection> to contain the quran section of the page
+        this.quranSection.setAttribute('class','quranSection') // set class name for the quranSection element
+
+
+        this.backButton = document.createElement('button') // create button element <backButton> to go back to the summury page
+        this.backButton.setAttribute('class','backButton')  // set class name for the backButton element
         this.backButton.textContent = 'Back'
         this.backButton.onclick = () => {
             while (body.firstChild) {
@@ -39,6 +65,9 @@ export class Page
             summury.showData(body)
             
         }
+
+
+
         body.appendChild(this.backButton)
         this.page.appendChild(this.quranSection)
         body.appendChild(this.page)
